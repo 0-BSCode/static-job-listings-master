431181ca13a9d944d081ab2464c70060d13085c3
Bryan Sanchez <bryanipsanchez@gmail.com>
2022-07-23 21:50:10 +0800

    Initialized project

---
+++ lib/styles.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+// Primary
+const COLOR_DARK_CYAN = HSLColor.fromAHSL(1, 180, 0.29, 0.50);
+
+// Secondary
+const COLOR_LIGHT_GRAY_CYAN_BACKGROUND = HSLColor.fromAHSL(1, 180, 0.52, 0.96);
+const COLOR_LIGHT_GRAY_CYAN_TABLETS = HSLColor.fromAHSL(1, 180, 0.31, 0.95);
+const COLOR_DARK_GRAY_CYAN = HSLColor.fromAHSL(1, 180, 0.08, 0.52);
+const COLOR_VERY_DARK_GRAY_CYAN = HSLColor.fromAHSL(1, 180, 0.14, 0.20);
