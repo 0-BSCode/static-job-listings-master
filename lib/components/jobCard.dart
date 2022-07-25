418c04a41125e61fa0a62ed640a84f93ca220a0d
Bryan Sanchez <bryanipsanchez@gmail.com>
2022-07-25 20:45:50 +0800

    Added card content

--- lib/components/jobCard.dart
+++ lib/components/jobCard.dart
@@ -17,32 +17,122 @@ class JobCard extends StatelessWidget {
       padding: EdgeInsets.fromLTRB(0, rootSize * 3, 0, 0),
       child: Container(
         height: rootSize * 12,
-        padding: EdgeInsets.symmetric(
-            horizontal: rootSize, vertical: rootSize * 1.5),
         decoration: BoxDecoration(
           color: Colors.white,
-          border: Border(
-            left: BorderSide(
-              color: COLOR_DARK_CYAN.toColor(),
-              width: rootSize * 5 / 15,
-              // style: BorderStyle.solid,
-            ),
-          ),
+          borderRadius: BorderRadius.circular(rootSize * 5 / 15),
         ),
-        child: Stack(
-          clipBehavior: Clip.none,
-          children: [
-            Positioned(
-              top: -rootSize * 3.5,
-              left: rootSize,
-              child: Image(
-                image: AssetImage(
-                  parseImagePath(jobInfo['logo']),
+        child: LayoutBuilder(
+          builder: (context, constraints) {
+            return Stack(
+              clipBehavior: Clip.none,
+              children: [
+                Positioned(
+                  top: -rootSize * 1.5,
+                  left: rootSize * 1.5,
+                  child: Image(
+                    image: AssetImage(
+                      parseImagePath(jobInfo['logo']),
+                    ),
+                    width: rootSize * 3.5,
+                  ),
                 ),
-                width: rootSize * 3.5,
-              ),
-            ),
-          ],
+                Row(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Container(
+                      width: rootSize * 5 / 15,
+                      decoration: BoxDecoration(
+                        color: COLOR_DARK_CYAN.toColor(),
+                        borderRadius: BorderRadius.only(
+                          topLeft: Radius.circular(rootSize * 5 / 15),
+                          bottomLeft: Radius.circular(rootSize * 5 / 15),
+                        ),
+                      ),
+                    ),
+                    Container(
+                      padding: EdgeInsets.symmetric(
+                          horizontal: rootSize * 1.5, vertical: rootSize * 3),
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Row(
+                            children: [
+                              Text(
+                                jobInfo['company'],
+                              ),
+                              if (jobInfo['new'])
+                                Container(
+                                  child: Text(
+                                    "NEW!",
+                                  ),
+                                ),
+                              if (jobInfo['featured'])
+                                Container(
+                                  child: Text(
+                                    'FEATURED',
+                                  ),
+                                ),
+                            ],
+                          ),
+                          Text(
+                            jobInfo['position'],
+                          ),
+                          Row(
+                            children: [
+                              Text(jobInfo['postedAt']),
+                              Text(
+                                ' • ',
+                              ),
+                              Text(
+                                jobInfo['contract'],
+                              ),
+                              Text(
+                                ' • ',
+                              ),
+                              Text(
+                                jobInfo['location'],
+                              ),
+                            ],
+                          ),
+                          Container(
+                            height: rootSize * 3 / 15,
+                            width: constraints.maxWidth - (rootSize * 4),
+                            color: COLOR_LIGHT_GRAY_CYAN_TABLETS.toColor(),
+                          ),
+                          Expanded(
+                            child: Row(
+                              children: [
+                                ListView.builder(
+                                  shrinkWrap: true,
+                                  scrollDirection: Axis.horizontal,
+                                  itemCount: jobInfo["languages"].length,
+                                  itemBuilder: (context, index) {
+                                    return Container(
+                                      child: Text(jobInfo["languages"][index]),
+                                    );
+                                  },
+                                ),
+                                ListView.builder(
+                                  shrinkWrap: true,
+                                  scrollDirection: Axis.horizontal,
+                                  itemCount: jobInfo["tools"].length,
+                                  itemBuilder: (context, index) {
+                                    return Container(
+                                      child: Text(jobInfo["tools"][index]),
+                                    );
+                                  },
+                                ),
+                              ],
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
+                  ],
+                )
+              ],
+            );
+          },
         ),
       ),
     );
