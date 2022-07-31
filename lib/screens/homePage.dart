6707cca1c1ff679d0423291a9835382f9bd3b6f2
Bryan Sanchez <bryanipsanchez@gmail.com>
2022-07-31 11:26:22 +0800

    Refactored home screen into stateless widget

--- lib/screens/homePage.dart
+++ lib/screens/homePage.dart
@@ -9,14 +9,9 @@ import 'package:flutter_svg/flutter_svg.dart';
 import 'package:static_job_listings_master/utils/generateFilters.dart';
 import 'package:static_job_listings_master/utils/readJSONFile.dart';
 
-class HomePage extends StatefulWidget {
-  HomePage({Key? key}) : super(key: key);
+class HomePage extends StatelessWidget {
+  const HomePage({Key? key}) : super(key: key);
 
-  @override
-  State<HomePage> createState() => _HomePageState();
-}
-
-class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
     final double rootSize = Provider.of<RootSizeProvider>(context).rootSize;
@@ -25,86 +20,88 @@ class _HomePageState extends State<HomePage> {
     final List<dynamic> filteredJobs =
         Provider.of<FiltersProvider>(context).filteredJobs;
     debugPrint("${deviceWidth}");
-    return SafeArea(child: Scaffold(
-      body: LayoutBuilder(
-        builder: (context, constraints) {
-          return Container(
-            width: constraints.maxWidth,
-            height: constraints.maxHeight,
-            color: COLOR_LIGHT_GRAY_CYAN_BACKGROUND.toColor(),
-            child: Stack(
-              children: [
-                Column(
-                  children: [
-                    Container(
-                      color: COLOR_DARK_CYAN.toColor(),
-                      child: SvgPicture.asset(
-                        "assets/images/bg-header-mobile.svg",
-                        semanticsLabel: "Background header mobile",
-                      ),
-                    ),
-                    Expanded(
-                      child: SingleChildScrollView(
-                        scrollDirection: Axis.vertical,
-                        physics: BouncingScrollPhysics(),
-                        child: Padding(
-                          padding: EdgeInsets.fromLTRB(
-                              rootSize * 1.5, rootSize * 2, rootSize * 1.5, 0),
-                          child: ListView.builder(
-                            shrinkWrap: true,
-                            scrollDirection: Axis.vertical,
-                            itemCount: filteredJobs.length,
-                            itemBuilder: (context, index) {
-                              return JobCard(jobInfo: filteredJobs[index]);
-                            },
-                          ),
-                        ),
-                      ),
-                    ),
-                  ],
-                ),
-                Container(
-                  decoration: BoxDecoration(
-                    color: Colors.white,
-                    borderRadius: BorderRadius.circular(rootSize * 5 / 15),
-                  ),
-                  margin: EdgeInsets.fromLTRB(
-                      rootSize * 1.5, rootSize * 8.5, rootSize * 1.5, 0),
-                  padding: EdgeInsets.symmetric(
-                      horizontal: rootSize, vertical: rootSize * 0.8),
-                  child: Row(
+    return SafeArea(
+      child: Scaffold(
+        body: LayoutBuilder(
+          builder: (context, constraints) {
+            return Container(
+              width: constraints.maxWidth,
+              height: constraints.maxHeight,
+              color: COLOR_LIGHT_GRAY_CYAN_BACKGROUND.toColor(),
+              child: Stack(
+                children: [
+                  Column(
                     children: [
-                      Expanded(
-                        child: Wrap(
-                          direction: Axis.horizontal,
-                          spacing: rootSize,
-                          runSpacing: rootSize,
-                          children: [
-                            ...generateFilters(filters),
-                          ],
+                      Container(
+                        color: COLOR_DARK_CYAN.toColor(),
+                        child: SvgPicture.asset(
+                          "assets/images/bg-header-mobile.svg",
+                          semanticsLabel: "Background header mobile",
                         ),
                       ),
-                      TextButton(
-                        onPressed: () {
-                          Provider.of<FiltersProvider>(context, listen: false)
-                              .clearFilters();
-                        },
-                        child: Text(
-                          'Clear',
-                          style: TextStyle(
-                            color: COLOR_DARK_GRAY_CYAN.toColor(),
-                            fontWeight: FontWeight.w700,
+                      Expanded(
+                        child: SingleChildScrollView(
+                          scrollDirection: Axis.vertical,
+                          physics: BouncingScrollPhysics(),
+                          child: Padding(
+                            padding: EdgeInsets.fromLTRB(rootSize * 1.5,
+                                rootSize * 2, rootSize * 1.5, 0),
+                            child: ListView.builder(
+                              shrinkWrap: true,
+                              scrollDirection: Axis.vertical,
+                              itemCount: filteredJobs.length,
+                              itemBuilder: (context, index) {
+                                return JobCard(jobInfo: filteredJobs[index]);
+                              },
+                            ),
                           ),
                         ),
                       ),
                     ],
                   ),
-                ),
-              ],
-            ),
-          );
-        },
+                  Container(
+                    decoration: BoxDecoration(
+                      color: Colors.white,
+                      borderRadius: BorderRadius.circular(rootSize * 5 / 15),
+                    ),
+                    margin: EdgeInsets.fromLTRB(
+                        rootSize * 1.5, rootSize * 8.5, rootSize * 1.5, 0),
+                    padding: EdgeInsets.symmetric(
+                        horizontal: rootSize, vertical: rootSize * 0.8),
+                    child: Row(
+                      children: [
+                        Expanded(
+                          child: Wrap(
+                            direction: Axis.horizontal,
+                            spacing: rootSize,
+                            runSpacing: rootSize,
+                            children: [
+                              ...generateFilters(filters),
+                            ],
+                          ),
+                        ),
+                        TextButton(
+                          onPressed: () {
+                            Provider.of<FiltersProvider>(context, listen: false)
+                                .clearFilters();
+                          },
+                          child: Text(
+                            'Clear',
+                            style: TextStyle(
+                              color: COLOR_DARK_GRAY_CYAN.toColor(),
+                              fontWeight: FontWeight.w700,
+                            ),
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                ],
+              ),
+            );
+          },
+        ),
       ),
-    ));
+    );
   }
 }
